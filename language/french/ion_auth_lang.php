93cc79327d0a7c0c514c126861d4209f77bfa3ca
Rot <rotinpain.rip@gmail.com>
2012-07-18 00:48:14 +0300

    Update 2

--- language/french/ion_auth_lang.php
+++ language/french/ion_auth_lang.php
@@ -7,18 +7,17 @@
 *
 * Location: http://github.com/benedmunds/ion_auth/
 *
-* Created: 03.23.2010
-*
+* Created:  03.14.2010
+* Last update: 17.07.2012 by rotinpain
 * Description: French language file for Ion Auth messages and errors
 *
 */
- 
+
 // Account Creation
 $lang['account_creation_successful'] = 'Compte créé avec succès';
 $lang['account_creation_unsuccessful'] = 'Impossible de créer le compte';
 $lang['account_creation_duplicate_email'] = 'Email déjà utilisé ou invalide';
 $lang['account_creation_duplicate_username'] = 'Nom d\'utilisateur déjà utilisé ou invalide';
- 
  
 // Password
 $lang['password_change_successful'] = 'Le mot de passe a été changé avec succès';
@@ -37,7 +36,7 @@ $lang['activation_email_unsuccessful'] = 'Impossible d\'envoyer le mail d\'activ
 // Login / Logout
 $lang['login_successful'] = 'Connecté avec succès';
 $lang['login_unsuccessful'] = 'Erreur lors de la connexion';
-$lang['login_unsuccessful_not_active'] = 'Ce compte est inactif';
+$lang['login_unsuccessful_not_active']  	 = 'Ce compte est inactif';
 $lang['logout_successful'] = 'Déconnexion effectuée avec succès';
   
 // Account Changes
@@ -46,7 +45,30 @@ $lang['update_unsuccessful'] = 'Impossible de mettre à jour le compte utilisate
 $lang['delete_successful'] = 'Utilisateur supprimé';
 $lang['delete_unsuccessful'] = 'Impossible de supprimer l\'utilisateur';
 
-// Email Subjects - TODO Please Translate
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_new_password_subject']          = 'New Password';
-$lang['email_activation_subject']            = 'Account Activation';
+// Email Subjects
+$lang['email_forgotten_password_subject']    = 'Vérification pour mot de passe oublié';
+$lang['email_new_password_subject']          = 'Nouveau mot de passe';
+$lang['email_activation_subject']            = 'Vérification pour l\'activation de compte'; 
+
+// Form fields and other texts
+$lang['text_identity']                       = 'Identité';
+$lang['text_password']                       = 'Mot de passe';
+$lang['text_password_old']                   = 'Ancien mot de passe';
+$lang['text_password_new']                   = 'Nouveau mot de passe';
+$lang['text_password_new_confirm']           = 'Confirmer le nouveau mot de passe';
+$lang['text_email']               = 'Adresse email';
+$lang['text_confirmation']               = 'Confirmation';
+$lang['text_user_id']               = 'ID utilisateur';
+
+$lang['text_first_name']               = 'Nom';
+$lang['text_last_name']               = 'Prénom';
+$lang['text_phone_part1']               = 'Tél part1';
+$lang['text_phone_part2']               = 'Tél part2';
+$lang['text_phone_part3']               = 'Tél part3';
+$lang['text_company_name']               = 'Structure';
+$lang['text_password_confirm']           = 'Confirmation du mot de passe';
+
+$lang['msg_user_created']           = 'Utilisateur créé';
+$lang['title_create_user']           = 'Créer un utilisateur';
+$lang['title_login']           = 'Connexion';
+$lang['title_logout']           = 'Me déconnecter';
